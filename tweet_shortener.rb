c6c29896090c68969abb666a1c5245b109562d2c
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-04 15:55:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
+def dictionary
+  hash = { "hello" => "hi",
+  "to" => "2"
+  "two" => "2"
+  "too" => "2"
+  "for" => "4"
+  "four" => "4"
+  "be" => "b"
+  "you" => "u"
+  "at" => "@"
+  "and" => "&"}
+end
+
+def word_substituter(tweet_one)
+  array = tweet_one.split(" ")
+  array.each_with_index do |word, index|
+    dictionary.keys.each do |x|
+      if x == word
+        array[index] = dictionary[x]
+      end
+    end
+  end
+  array.join(" ")
 # Write your code here.
